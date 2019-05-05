013e6831dd4e605f34777bee3a81a357be44f702
zl <1834448205@163.com>
2019-05-05 18:01:07 +0800

    05.05

--- demo/新建文件夹/Allez.js
+++ demo/新建文件夹/Allez.js
@@ -1,17 +1,19 @@
 var log = function() {
     console.log.apply(console, arguments)
 }
-//返回现在日期  str间隔符 格式2019str04str20
-var showNowTime = function(str) {
+//返回现在日期  Separator分隔符符 格式2019sep04sep20
+var show_nowTime = function(sep="-") {
     var myDate = new Date()
     var nowYear = myDate.getFullYear()
     var nowMonth =myDate.getMonth() + 1
-    for(let i = 1; i < 10; i=i+1){
-        if(nowMonth == i) {
-            nowMonth = "0" + nowMonth
-        }
-    }
     var nowToday = myDate.getDate()
-    var nowTime = nowYear + str + nowMonth + str + nowToday;
+    var nowTime = ""
+    if(nowMonth <= 9) {
+        nowMonth = "0" + nowMonth
+    }
+    if(nowToday <= 9) {
+        nowToday = "0" + nowToday
+    }
+    nowTime = nowYear + sep + nowMonth + sep + nowToday;
     return nowTime
 }
